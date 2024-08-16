537304232d78c0f32f454d6eea08c7a562152fe4
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2024-08-16 14:52:51 +0500

    beta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
 import { Button } from "@/components/ui/button";
-import { MdOutlineSchool } from "react-icons/md";
-import Image from "next/image";
+import { cn } from "@/lib/utils";
 
 export default function Home() {
   return (
     <>
-      <header className="">
+      {/* <header className="">
         <div className="p-4 max-w-screen-xl mx-auto">
-          <h1>This is logo</h1>
+          <h1 className=""></h1>
         </div>
-      </header>
+      </header> */}
 
       {/* hero section */}
       <main className=" text-white p-4 min-h-[80vh] h-[80vh] flex flex-col relative">
@@ -45,8 +44,9 @@ export default function Home() {
         <div className="h-6"></div>
       </main>
 
+      {/* cards */}
       <div
-        className="max-w-screen-xl grid  mx-auto p-2 gap-2  -translate-y-10  overflow-x-auto"
+        className="max-w-[1200px] grid  mx-auto p-2 gap-2  -translate-y-10  overflow-x-auto"
         style={{
           gridTemplateColumns: "repeat(3, minmax(250px, 1fr))"
         }}
@@ -59,23 +59,34 @@ export default function Home() {
           return (
             <div
               key={i}
-              className="w-full bg-blue-900 text-white p-4 md:p-8 rounded-md "
+              className={cn(
+                "w-full bg-blue-900 text-white p-4 md:p-8 rounded-md ",
+                i === 0 && "bg-gradient-to-b from-pink-500 to-pink-600",
+                i === 1 && "bg-gradient-to-b from-blue-800 to-blue-900",
+                i === 2 && "bg-gradient-to-b from-cyan-500 to-cyan-600"
+              )}
             >
               <img src={imgSrc} alt="kids icon" width={"60"} height={"60"} />
-              <h1 className="font-bold text-2xl my-4">SCHOOL</h1>
+              <h1 className="font-bold text-2xl my-4">
+                {i === 0
+                  ? "Empowering Education"
+                  : i === 1
+                  ? "Health and Well-being"
+                  : "Emotional Support"}
+              </h1>
               <p className="text-sm mb-2">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Praesentium mollitia asperiores culpa voluptate aspernatur
-                facere iste corporis ipsa, voluptatibus sit illum officiis
-                perferendis ducimus quod expedita, magnam minima architecto
-                eaque!
+                {i === 0
+                  ? "Your generosity ensures that every child under our care has access to quality education. By unlocking the doors to learning, we break the cycle of poverty and open up a world of opportunities for these children."
+                  : i === 1
+                  ? "With your help, we provide essential healthcare services and nutritious meals, creating a safe and nurturing environment where children can thrive both physically and mentally."
+                  : "Your donation enables us to offer counseling services and emotional support, helping children heal from trauma and build resilience."}
               </p>
             </div>
           );
         })}
       </div>
 
-      <section className="max-w-screen-xl mx-auto p-4 mt-24">
+      <section className="max-w-screen-xl mx-auto p-4 mt-12 md:mt-24">
         <div className="bg-gray-100 relative p-6 md:p-12 grid grid-cols-1 md:grid-cols-2 gap-4">
           {/* Decorative square */}
           <div className="absolute w-16 h-16 bg-white left-0 rotate-45 -translate-y-1/2 -translate-x-1/2 top-0"></div>
@@ -83,16 +94,17 @@ export default function Home() {
           {/* Content Section */}
           <div>
             <h2 className="font-bold text-cyan-600 text-sm my-1">
-              EL TUNCO REMAR
+              Begum Inayat
             </h2>
             <h1 className="text-4xl font-black mb-6 text-blue-950">
               Our Mission
             </h1>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
-              similique aspernatur iure consequatur dolore omnis distinctio
-              ullam, quos consequuntur aliquid alias repellat? Eveniet
-              doloremque ratione, perferendis odio quisquam libero repellendus.
+              At Begum Inayat Welfare Society, we believe that every child
+              deserves a bright future filled with love, education, and
+              opportunity. Our mission is to provide orphaned children with a
+              nurturing environment where they can grow, learn, and develop the
+              essential skills necessary for success in life.
             </p>
           </div>
 
@@ -111,19 +123,24 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="mt-24 bg-light-green p-12">
-        <div className="max-w-screen-xl mx-auto grid grid-cols-2 gap-2 p-12">
-          <div></div>
+      <section className="mt-24 md:mt-32 bg-light-green p-12">
+        <div className="max-w-screen-xl mx-auto grid md:grid-cols-2 gap-6 md:p-12">
           <div>
-            <h2 className="font-bold text-white text-sm my-1">BAKERY</h2>
+            <img
+              src="/images/sample-house.jpeg"
+              alt="sample house picture"
+              className="rounded-md sm:w-[100%] md:w-[450px] md:h-[120%] md:translate-y-[-25%]"
+            />
+          </div>
+          <div>
+            <h2 className="font-bold text-white text-sm my-1">HOUSE</h2>
             <h1 className="text-white text-4xl font-black mb-6">
-              Cake & Amigos
+              Building Dreams
             </h1>
             <p className="text-white">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
-              similique aspernatur iure consequatur dolore omnis distinctio
-              ullam, quos consequuntur aliquid alias repellat? Eveniet
-              doloremque ratione, perferendis odio quisquam libero repellendus.
+              Your support enables us to expand our facilities and reach even
+              more orphaned children in need. Together, we ensure that no child
+              is left behind, and every dream has the chance to flourish.
             </p>
 
             <Button className="mt-8" variant={"outline"} size={"lg"}>
@@ -134,17 +151,18 @@ export default function Home() {
       </section>
 
       <section className="max-w-screen-xl mx-auto mt-24">
-        <div className="p-12 bg-gray-100 rounded-md grid grid-cols-2">
+        <div className="p-6 md:p-12 bg-gray-100 rounded-md grid md:grid-cols-2 gap-6">
           <div>
             <h1 className="to-blue-950 text-4xl font-black mb-6">
               Ready to help?
             </h1>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
-              similique aspernatur iure consequatur dolore omnis distinctio
-              ullam.
+              Your contribution, no matter how small, makes a meaningful
+              difference in the lives of orphaned children. By joining us, you
+              become a beacon of hope, guiding these children toward brighter
+              tomorrows.
             </p>
-            <div className="mt-8 flex gap-4">
+            <div className="mt-8 flex gap-4 flex-wrap">
               <Button
                 className="text-white font-bold"
                 variant={"brand"}
@@ -161,6 +179,13 @@ export default function Home() {
               </Button>
             </div>
           </div>
+          <div className="flex">
+            <img
+              src="/images/sample-kids-drawing.jpg"
+              className="rounded-md sm:w-[100%] md:w-[450px] ml-auto object-cover"
+              alt="kids drawing"
+            />
+          </div>
         </div>
       </section>
 
@@ -170,7 +195,9 @@ export default function Home() {
             <h1 className="text-cyan-700 font-bold text-2xl mb-2">
               Begum Inayat
             </h1>
-            <p>A open source organization for orphan adoption</p>
+            <p>
+              A charitable organization dedicated to orphan care and adoption.
+            </p>
           </div>
           <div className="flex flex-col items-center">
             <div className="max-md:mr-auto">
@@ -190,7 +217,7 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="mt-4 max-w-screen-xl mx-auto border-t border-gray-200 py-4 flex">
+        <div className="mt-4 max-w-screen-xl mx-auto border-t border-gray-200 py-4 px-2 flex">
           <p className="ml-auto text-gray-500">&copy; copyright 2024</p>
         </div>
       </footer>
