6c843f83294ebfc30f3739a4b7aca20926c27713
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2024-08-17 21:32:08 +0500

    add loco motive scroll

---
+++ src/providers/locomotive.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { ReactNode, useLayoutEffect } from "react";
+
+export default function LocoMotiveProvider({
+  children
+}: {
+  children: ReactNode;
+}) {
+  useLayoutEffect(() => {
+    const importLocoMotive = async () => {
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+      const locoScroll = new LocomotiveScroll({});
+    };
+
+    importLocoMotive();
+  }, []);
+
+  return <>{children}</>;
+}
