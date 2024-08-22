f1c0e399d613a14a60ae0b5ca4c07bbe945f1963
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2024-08-22 19:28:31 +0500

    done with messages & cta section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,35 +20,50 @@ export default function Home() {
       <OurMission />
 
       <section className="mt-24 md:mt-32 bg-light-green p-6 md:p-12">
-        <div className="max-w-screen-xl mx-auto grid md:grid-cols-2 gap-6 md:p-12">
+        <div className="max-w-screen-xl mx-auto grid md:grid-cols-2 gap-6 md:p-12 relative">
           <div>
             <img
-              src="/images/sample-house.jpeg"
+              src="/images/children-group-photo.jpeg"
               alt="sample house picture"
-              className="rounded-md sm:w-[100%] md:w-[450px] md:h-[120%] md:translate-y-[-25%]"
+              className="rounded-md sm:w-[100%] md:w-[450px] md:h-[100%] "
             />
           </div>
-          <div>
-            <h2 className="font-bold text-white text-sm my-1">HOUSE</h2>
-            <h1 className="text-white text-4xl font-black mb-6">
-              Building Dreams
-            </h1>
-            <p className="text-white">
-              Your support enables us to expand our facilities and reach even
-              more orphaned children in need. Together, we ensure that no child
-              is left behind, and every dream has the chance to flourish.
-            </p>
+          <div className="flex flex-col justify-evenly">
+            <img
+              src="/images/art/cute-doodle.png"
+              className="right-[30%] bottom-[20%] lg:w-[300px] mb-4"
+              width={200}
+              height={200}
+            />
+            <div>
+              {/* <h2 className="font-bold text-white text-sm my-1">HOUSE</h2> */}
+              <h1 className="text-white text-4xl font-black mb-6">
+                Building Dreams
+              </h1>
+              <p className="text-white">
+                Your support enables us to expand our facilities and reach even
+                more orphaned children in need. Together, we ensure that no
+                child is left behind, and every dream has the chance to
+                flourish.
+              </p>
 
-            <Button className="mt-8" variant={"outline"} size={"lg"}>
-              LEARN MORE
-            </Button>
+              <Button className="mt-8" variant={"outline"} size={"lg"}>
+                LEARN MORE
+              </Button>
+            </div>
           </div>
         </div>
       </section>
 
-      <section className="max-w-screen-xl mx-auto mt-24">
-        <div className="p-6 md:p-12 bg-gray-100 rounded-md grid md:grid-cols-2 gap-6">
-          <div>
+      <section className="max-w-screen-xl mx-auto mt-24 relative z-10">
+        <img
+          src="/images/art/heart.png"
+          className=" absolute left-4 xl:-translate-x-[34%] top-0 -translate-y-[30%] -rotate-12"
+          width={100}
+          height={100}
+        />
+        <div className="p-6  md:p-12 bg-gray-100 rounded-md grid md:grid-cols-2 gap-6">
+          <div className="z-10 mt-10">
             <h1 className="to-blue-950 text-4xl font-black mb-6">
               Ready to help?
             </h1>
