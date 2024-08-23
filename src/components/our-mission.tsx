43e953d6e006d70cae8f58dceeb4ac1b6c9f4bbb
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2024-08-23 10:09:41 +0500

    flower

--- src/components/our-mission.tsx
+++ src/components/our-mission.tsx
@@ -1,15 +1,23 @@
 export default function OurMission() {
   return (
     <section
-      className="max-w-screen-xl mx-auto p-4 mt-12 md:mt-24"
+      className="max-w-screen-xl relative mx-auto p-4 mt-12 md:mt-24"
       id="our-mission"
     >
+      <div className="absolute top-0 z-10 md:left-0 right-0">
+        <img
+          src="/images/art/flower.png"
+          alt="flower art"
+          width={100}
+          height={100}
+        />
+      </div>
       <div className="bg-gray-100 relative p-6 md:p-12 grid grid-cols-1 md:grid-cols-2 gap-4">
         {/* Decorative square */}
         <div className="absolute w-16 h-16 bg-white left-0 rotate-45 -translate-y-1/2 -translate-x-1/2 top-0"></div>
 
         {/* Content Section */}
-        <div>
+        <div className="z-20 mt-8">
           <h2 className="font-bold text-cyan-600 text-sm my-1">Begum Inayat</h2>
           <h1 className="text-4xl font-black mb-6 text-blue-950">
             Our Mission
